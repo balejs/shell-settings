91b812bd23e3ac546ea2994ca218b62d0c46e40c
Marco Ballesio <marco.ballesio@gmail.com>
2025-03-04 04:23:06 -0800

    setup: move from vundle to plug

--- setup.sh
+++ setup.sh
@@ -2,22 +2,29 @@
 
 #install oh-my-zsh
 ##NOTE: you need to exit from the shell that spawns automatically for the config to complete
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+#sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
-  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp vimrc ~/.vimrc
+
+if [ ! -d ~/.vim/autoload/plug.vim ]; then
+   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 vim +PluginInstall +qall
 
-~/.vim/bundle/fzf/install
+~/.vim/plugged/fzf/install
 
 #TODO: script to differentiate between macosx and Linux configs
 
 mkdir -p $HOME/.local/bin
 cat zshrc_additions >> $HOME/.zshrc
 
-cp vimrc ~/.vimrc
 mkdir -p ~/.vim/colors
 cp diff_colors.vim ~/.vim/colors
 cp gitconfig ~/.gitconfig
 cp tmux.conf ~/.tmux.conf
+
+#install homebrew
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
